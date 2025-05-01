592e0b6af39c7b98c1b9be7b66d5c7779e18bebb
Diego Rojas <devup2332@gmail.com>
2025-05-01 01:44:18 -0500

    master: Changed banner h to dvh

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -26,7 +26,7 @@ const links = [
 const Banner = () => {
   const { t } = useTranslation();
   return (
-    <div className="h-screen grid place-items-center" id="banner">
+    <div className="h-dvh grid place-items-center" id="banner">
       <div className="text-text-1 -translate-y-4 lg:flex">
         <div className="lg:w-6/12 lg:flex lg:flex-col lg:justify-center xl:w-5/12">
           <h1 className="text-5xl font-bold lg:text-6xl xl:text-7xl">
